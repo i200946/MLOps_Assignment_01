8e18a7394b74f139398f40d24f975ac299bd9911
Abdul Rehman Qasim <117030441+i200946@users.noreply.github.com>
2025-09-20 09:12:34 +0500

    Update app.py
    changed it to remove linting error, structure only

--- app/app.py
+++ app/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
+# Create a Flask app instance
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
+    """Route for the home page."""
     return "Hello, CI/CD pipeline is working!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
+if __name__ == "__main__":
+    # Run the app in debug mode
+    app.run(debug=True)
